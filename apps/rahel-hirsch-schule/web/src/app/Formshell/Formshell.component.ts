e1ba8283af3fa78b097ec1b736e6dc50c934221e
Eter Artmeladze <etoartmeladze@yahoo.com>
2025-01-21 11:17:50 +0100

    Componente für unterschiedliche Formular Felder erstellt

--- apps/rahel-hirsch-schule/web/src/app/Formshell/Formshell.component.ts
+++ apps/rahel-hirsch-schule/web/src/app/Formshell/Formshell.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import { ButtonModule } from 'primeng/button';
+
 @Component({
-  imports: [RouterModule],
+  imports: [RouterModule, ButtonModule],
   selector: 'app-root',
   templateUrl: './Formshell.component.html',
   styleUrl: './Formshell.component.css',
