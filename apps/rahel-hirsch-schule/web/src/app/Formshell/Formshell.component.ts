0afdd0127d16500a907b2c6c0f6e54656dcc01c9
Eter Artmeladze <etoartmeladze@yahoo.com>
2025-01-22 13:51:18 +0100

    Created header Component.

--- apps/rahel-hirsch-schule/web/src/app/Formshell/Formshell.component.ts
+++ apps/rahel-hirsch-schule/web/src/app/Formshell/Formshell.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
 import { ButtonModule } from 'primeng/button';
+import { HeaderComponent } from './Header/Header.component';
 
 @Component({
-  imports: [RouterModule, ButtonModule],
+  imports: [RouterModule, ButtonModule, HeaderComponent],
   selector: 'app-root',
   templateUrl: './Formshell.component.html',
   styleUrl: './Formshell.component.css',
