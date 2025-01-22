a2ba712802d0ba235281375f58f39da5cc3cc01b
Gabriel Augustinovic <gabriel.augustinovic@imago-images.de>
2025-01-22 15:40:09 +0100

    chore: add person schema

--- models/rahel-hirsch/database/days.schema.ts
+++ models/rahel-hirsch/database/days.schema.ts
@@ -1,12 +1,12 @@
 import { mysqlTable, int } from 'drizzle-orm/mysql-core';
 
-const daysSchema = mysqlTable('Days', {
-  daysId: int('daysId').primaryKey(),
+export const daysSchema = mysqlTable('Days', {
+  DaysId: int('DaysId').primaryKey(),
   monday: int('monday').notNull(),
   tuesday: int('tuesday').notNull(),
   wednesday: int('wednesday').notNull(),
   thursday: int('thursday').notNull(),
   friday: int('friday').notNull(),
   places: int('places').notNull(),
-  takenPlaces: int('takenPlaces').default(null),
+  takenPlaces: int('takenPlaces'),
 });
