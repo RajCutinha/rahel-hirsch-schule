3cddb1c4603e75ef13102ac25af18a996dd54a2c
Raj Cutinha <rajath.cutinha@protonmail.com>
2025-01-23 13:41:34 +0100

    refactor: streamline appRoutes by removing redundant NavigationComponent entry

--- apps/rahel-hirsch-schule/web/src/app/app.routes.ts
+++ apps/rahel-hirsch-schule/web/src/app/app.routes.ts
@@ -1,14 +1,9 @@
 import { Route } from '@angular/router';
 import { FormshellComponent } from './Formshell/Formshell.component';
-import { NavigationComponent } from './Navigation/Navigation.component'
 
 export const appRoutes: Route[] = [
   {
     path: '',
-    component: FormshellComponent,
-  },
-  {
-    path: '',
-    component: NavigationComponent,
-  },
+    component: FormshellComponent
+  }
 ];
