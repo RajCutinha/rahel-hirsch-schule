d8e4f4d9d02003f366c76803cfd6ea82df78c210
Gabriel Augustinovic <gabriel.augustinovic@imago-images.de>
2025-02-18 12:21:11 +0100

    fufufufuf

--- models/rahel-hirsch/database/schemas/EducationCoursesCompleted.schema.ts
+++ models/rahel-hirsch/database/schemas/EducationCoursesCompleted.schema.ts
@@ -3,12 +3,12 @@ import { mysqlTable, int } from 'drizzle-orm/mysql-core';
 import { Graduations_Schema } from './Graduations.schema';
 import { CourseCompletions_Schema } from './CourseCompletions.schema';
 
-export const EducationCoursesCompleted_Schema = mysqlTable('EducationCoursesCompleted', {
-    EducationCoursesCompletedId: int('EducationCoursesCompletedId').primaryKey().autoincrement(),
+export const EducationCoursesCompleted_Schema = mysqlTable('EduCourseComplete', {
+    EducationCoursesCompletedId: int('EduCourseCompleteId').primaryKey().autoincrement(),
     GraduationId: int('GraduationId')
         .notNull()
         .references(() => Graduations_Schema.GraduationId),
-    CourseCompletedId: int('CourseCompletedId')
+    CourseCompletedId: int('CourseCompleteId')
         .notNull()
         .references(() => CourseCompletions_Schema.CourseCompletedId)
 });
