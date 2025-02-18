bbff31690465612b6013ab0a79e0c444168dec4c
Gabriel Augustinovic <gabriel.augustinovic@imago-images.de>
2025-02-18 12:57:27 +0100

    fix: table foreign name

--- models/rahel-hirsch/database/schemas/RegistrationsStatuses.schema.ts
+++ models/rahel-hirsch/database/schemas/RegistrationsStatuses.schema.ts
@@ -1,13 +1,27 @@
-import { mysqlTable, int, mysqlEnum, timestamp } from 'drizzle-orm/mysql-core';
-
+import { mysqlTable, int, mysqlEnum, timestamp, foreignKey } from 'drizzle-orm/mysql-core';
 import { Registrations_Schema } from './Registrations.schema';
 
-export const RegistrationsStatuses_Schema = mysqlTable('RegistrationsStatuses', {
-    RegistrationStatusId: int('RegistrationStatusId').primaryKey().autoincrement(),
-    RegistrationId: int('RegistrationId')
-        .notNull()
-        .references(() => Registrations_Schema.RegistrationId),
-    status: mysqlEnum('status', ['Angenommen', 'Abgelehnt', 'In Bearbeitung', 'Neu']).default('Neu').notNull(),
-    createDate: timestamp('createDate').defaultNow().notNull(),
-    editDate: timestamp('editDate')
-});
+export const RegistrationsStatuses_Schema = mysqlTable(
+    'RegistrationsStatuses',
+    {
+        RegistrationStatusId: int('RegistrationStatusId')
+            .primaryKey()
+            .autoincrement(),
+
+        RegistrationId: int('RegistrationId').notNull(),
+        status: mysqlEnum('status', ['Angenommen', 'Abgelehnt', 'In Bearbeitung', 'Neu'])
+            .default('Neu')
+            .notNull(),
+        createDate: timestamp('createDate').defaultNow().notNull(),
+        editDate: timestamp('editDate'),
+    },
+    (table) => {
+        return {
+            fk_Registration: foreignKey({
+                name: 'fk_rstatus_registration', // Custom FK name
+                columns: [table.RegistrationId],
+                foreignColumns: [Registrations_Schema.RegistrationId],
+            }),
+        };
+    }
+);
