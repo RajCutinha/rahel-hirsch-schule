3b8b2f552f11b2c3fe8c254b53da91fe7d2f7dde
Gabriel Augustinovic <gabriel.augustinovic@imago-images.de>
2025-02-18 12:55:36 +0100

    fix: table foreign name

--- models/rahel-hirsch/database/schemas/MissingDocumentsListRegistrations.schema.ts
+++ models/rahel-hirsch/database/schemas/MissingDocumentsListRegistrations.schema.ts
@@ -1,17 +1,31 @@
-import { mysqlTable, int, tinyint } from 'drizzle-orm/mysql-core';
-
+import { mysqlTable, int, tinyint, foreignKey } from 'drizzle-orm/mysql-core';
 import { Registrations_Schema } from './Registrations.schema';
 import { MissingDocumentsList_Schema } from './MissingDocumentsList.schema';
 
-export const MissingDocumentsListRegistrations_Schema = mysqlTable('MissingDocListRegistration', {
-    MissingDocListRegistrationId: int('MissingDocListRegistrationId').primaryKey().autoincrement(),
-    
-    RegistrationId: int('RegistrationId')
-        .notNull()
-        .references(() => Registrations_Schema.RegistrationId),
+export const MissingDocumentsListRegistrations_Schema = mysqlTable(
+    'MissingDocListRegistration',
+    {
+        MissingDocListRegistrationId: int('MissingDocListRegistrationId')
+            .primaryKey()
+            .autoincrement(),
 
-    MissingDocumentListId: int('MissingDocListId')
-        .notNull()
-        .references(() => MissingDocumentsList_Schema.MissingDocumentListId),
-    missingDocument: tinyint('missingDoc').default(1)
-});
+        RegistrationId: int('RegistrationId').notNull(),
+        MissingDocumentListId: int('MissingDocListId').notNull(),
+        missingDocument: tinyint('missingDoc').default(1),
+    },
+    (table) => {
+        return {
+            fk_Registration: foreignKey({
+                name: 'fk_mdlr_registration', // Custom FK name
+                columns: [table.RegistrationId],
+                foreignColumns: [Registrations_Schema.RegistrationId],
+            }),
+
+            fk_MissingDocumentList: foreignKey({
+                name: 'fk_mdlr_missing_doc_list', // Custom FK name
+                columns: [table.MissingDocumentListId],
+                foreignColumns: [MissingDocumentsList_Schema.MissingDocumentListId],
+            }),
+        };
+    }
+);
