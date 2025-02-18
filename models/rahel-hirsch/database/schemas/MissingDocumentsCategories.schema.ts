2be50291030efc4a561bf3fab34e8903eba06785
Gabriel Augustinovic <gabriel.augustinovic@imago-images.de>
2025-02-18 12:51:15 +0100

    fix: foreign key table name

--- models/rahel-hirsch/database/schemas/MissingDocumentsCategories.schema.ts
+++ models/rahel-hirsch/database/schemas/MissingDocumentsCategories.schema.ts
@@ -1,11 +1,23 @@
-import { mysqlTable, int, varchar } from 'drizzle-orm/mysql-core';
-
+import { mysqlTable, int, varchar, foreignKey } from 'drizzle-orm/mysql-core';
 import { MissingDocumentsList_Schema } from './MissingDocumentsList.schema';
 
-export const MissingDocumentsCategorys_Schema = mysqlTable('MissingDocumentsCategories', {
-    MissingDocumentCategoryId: int('MissingDocumentCategoryId').primaryKey().autoincrement(),
-    MissingDocumentListId: int('MissingDocumentListId')
-        .notNull()
-        .references(() => MissingDocumentsList_Schema.MissingDocumentListId),
-    description: varchar('description', { length: 255 }).notNull()
-});
+export const MissingDocumentsCategorys_Schema = mysqlTable(
+    'MissingDocumentsCategories',
+    {
+        MissingDocumentCategoryId: int('MissingDocumentCategoryId')
+            .primaryKey()
+            .autoincrement(),
+
+        MissingDocumentListId: int('MissingDocumentListId').notNull(),
+        description: varchar('description', { length: 255 }).notNull(),
+    },
+    (table) => {
+        return {
+            fk_MissingDocumentList: foreignKey({
+                name: 'fk_mdc_missing_doc_list', // Custom FK name
+                columns: [table.MissingDocumentListId],
+                foreignColumns: [MissingDocumentsList_Schema.MissingDocumentListId],
+            }),
+        };
+    }
+);
