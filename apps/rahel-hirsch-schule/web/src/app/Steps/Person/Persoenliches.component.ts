5d78c155462e926519026eb7c52960be8821cf96
Raj Cutinha <rajath.cutinha@protonmail.com>
2025-02-22 16:47:56 +0100

    wip: add form binding for persoenliches

--- apps/rahel-hirsch-schule/web/src/app/Steps/Person/Persoenliches.component.ts
+++ apps/rahel-hirsch-schule/web/src/app/Steps/Person/Persoenliches.component.ts
@@ -1,18 +1,24 @@
 import { Component } from '@angular/core';
+import { ControlContainer, ReactiveFormsModule, FormGroup } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 
-
-
 @Component({
-  imports: [RouterModule, ButtonModule, InputTextModule],
+  imports: [RouterModule, ButtonModule, InputTextModule, ReactiveFormsModule],
   selector: 'app-persoenliches',
   templateUrl: './Persoenliches.component.html',
-  styleUrl: './Persoenliches.component.scss',
-
+  styleUrl: './Persoenliches.component.scss'
 })
 export class PersoenlichesComponent {
-  
+  constructor(private controlContainer: ControlContainer) {}
 
+  get parentForm() {
+    return this.controlContainer.control;
+  }
+
+  get personForm() {
+    return this.parentForm?.get('person') as FormGroup;
+  }
 }
