b02ed7ff6b5b90641ebe2eb1dbd31687a47e3871
Raj Cutinha <rajath.cutinha@protonmail.com>
2025-02-22 17:31:34 +0100

    wip: add form bindings to Unter18 component

--- apps/rahel-hirsch-schule/web/src/app/Steps/Unter18/Unter18.component.ts
+++ apps/rahel-hirsch-schule/web/src/app/Steps/Unter18/Unter18.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { ControlContainer, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
-    imports: [RouterModule, ButtonModule, InputTextModule],
+    imports: [RouterModule, ButtonModule, InputTextModule, ReactiveFormsModule],
     selector: 'app-unter18',
     templateUrl: './Unter18.component.html',
     styleUrl: './Unter18.component.scss'
 })
-export class Unter18Component {}
+export class Unter18Component {
+  constructor(private controlContainer: ControlContainer) {}
+
+  get parentForm() {
+    return this.controlContainer.control as FormGroup;
+  }
+
+  get motherForm() {
+    return this.parentForm?.get('mother') as FormGroup;
+  }
+
+  get fatherForm() {
+    return this.parentForm?.get('father') as FormGroup;
+  }
+
+  get guardianForm() {
+    return this.parentForm?.get('guardian') as FormGroup;
+  }
+
+  get guardianAddressControl() {
+    return this.parentForm?.get('guardianAddress') as FormControl;
+  }
+}
