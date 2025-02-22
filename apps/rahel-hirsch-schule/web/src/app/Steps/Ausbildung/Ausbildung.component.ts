8e149f8017af75a7eaf2a7cbd22649fd9cf3e0bd
Raj Cutinha <rajath.cutinha@protonmail.com>
2025-02-22 23:21:59 +0100

    wip: add form bindings and file upload to ausbildung component

--- apps/rahel-hirsch-schule/web/src/app/Steps/Ausbildung/Ausbildung.component.ts
+++ apps/rahel-hirsch-schule/web/src/app/Steps/Ausbildung/Ausbildung.component.ts
@@ -1,12 +1,70 @@
 import { Component } from '@angular/core';
+import { ControlContainer, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
 import { RouterModule } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
-    imports: [RouterModule, ButtonModule, InputTextModule],
+    imports: [RouterModule, ButtonModule, InputTextModule, ReactiveFormsModule],
     selector: 'app-ausbildung',
     templateUrl: './Ausbildung.component.html',
     styleUrl: './Ausbildung.component.scss'
 })
-export class AusbildungComponent {}
+export class AusbildungComponent {
+
+  schooldays = [
+    { value: 'Mo/Di', label: 'Mo/Di' },
+    { value: 'Di/Mi', label: 'Di/Mi' },
+    { value: 'Di/Fr', label: 'Di/Fr' },
+    { value: 'Mo/Do', label: 'Mo/Do' },
+    { value: 'Mo/Fr', label: 'Mo/Fr' },
+    { value: 'Mo/Mi', label: 'Mo/Mi' },
+    { value: 'Do/Fr', label: 'Do/Fr' },
+    { value: 'Mi/Do', label: 'Mi/Do' }
+  ];
+
+  constructor(private controlContainer: ControlContainer) {}
+
+  get parentForm() {
+    return this.controlContainer.control as FormGroup;
+  }
+
+  get employerForm() {
+    return this.parentForm?.get('employer') as FormGroup;
+  }
+
+  get schooldaysControl() {
+    return this.parentForm?.get('schooldays') as FormControl;
+  }
+
+  get contractFileControl() {
+    return this.parentForm?.get('contractFile') as FormControl;
+  }
+
+  onFileUpload(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const file = input.files?.item(0);
+
+    if (file) {
+      this.contractFileControl?.setValue(file);
+    } else {
+      this.contractFileControl?.setValue(null);
+    }
+  }
+
+  updateSchooldays(event: Event) {
+    const target = event.target as HTMLInputElement;
+
+    if (!target) return;
+
+    const value = target.value;
+    const currentValues = this.schooldaysControl.value || [];
+
+    if (target.checked) {
+      this.schooldaysControl.setValue([...currentValues, value]);
+    } else {
+      this.schooldaysControl.setValue(currentValues.filter((v: string) => v !== value));
+    }
+  }
+}
