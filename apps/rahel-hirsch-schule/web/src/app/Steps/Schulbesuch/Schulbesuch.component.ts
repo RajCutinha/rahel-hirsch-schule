f8462ecfd604872d25f488e84200b52eefbc8ec6
Raj Cutinha <rajath.cutinha@protonmail.com>
2025-02-22 23:39:54 +0100

    feat: add reactive form bindings to Schulbesuch component

--- apps/rahel-hirsch-schule/web/src/app/Steps/Schulbesuch/Schulbesuch.component.ts
+++ apps/rahel-hirsch-schule/web/src/app/Steps/Schulbesuch/Schulbesuch.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
+import { ControlContainer, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
-  imports: [],
+  imports: [ButtonModule, InputTextModule, ReactiveFormsModule],
   selector: 'app-schulbesuch',
   templateUrl: './Schulbesuch.component.html',
-  styleUrl: './Schulbesuch.component.scss',
-
+  styleUrl: './Schulbesuch.component.scss'
 })
 export class SchulbesuchComponent {
+  constructor(private controlContainer: ControlContainer) {}
 
+  get parentForm() {
+    return this.controlContainer.control as FormGroup;
+  }
 
+  get educationForm() {
+    return this.parentForm?.get('education') as FormGroup;
+  }
 }
