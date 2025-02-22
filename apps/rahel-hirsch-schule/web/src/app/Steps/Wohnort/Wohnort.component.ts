4348db3323dbc51bb683d11836e063e5287494f3
Raj Cutinha <rajath.cutinha@protonmail.com>
2025-02-22 16:49:13 +0100

    wip: add form bindings to wohnort component

--- apps/rahel-hirsch-schule/web/src/app/Steps/Wohnort/Wohnort.component.ts
+++ apps/rahel-hirsch-schule/web/src/app/Steps/Wohnort/Wohnort.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { ControlContainer, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
 import { RouterModule } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
-    imports: [RouterModule, ButtonModule, InputTextModule],
+    imports: [RouterModule, ButtonModule, InputTextModule, ReactiveFormsModule],
     selector: 'app-wohnort',
     templateUrl: './Wohnort.component.html',
     styleUrl: './Wohnort.component.scss'
 })
-export class WohnortComponent {}
+export class WohnortComponent {
+    constructor(private controlContainer: ControlContainer) {}
+
+    get parentForm() {
+        return this.controlContainer.control;
+    }
+
+    get personPlaceForm() {
+        return this.parentForm?.get('personPlace') as FormGroup;
+    }
+
+    get emergencyContactForm() {
+      return this.parentForm?.get('emergencyContact') as FormGroup;
+    }
+}
